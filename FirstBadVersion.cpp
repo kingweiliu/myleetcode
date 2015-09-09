59903119f881423fff99afcb031bd8fc1f489a93
U-ljw-PC\ljw <kingweiliu@gmail.com>
2015-09-09 16:02:43 +0800

    for https://leetcode.com/problems/first-bad-version/

---
+++ FirstBadVersion.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+private:
+
+    int findBadVersion(int l, int r) {
+	std::cout << "input:" << l << ", " << r << std::endl;
+        if (l >= r) {
+            return r;
+        }
+        int mid = l / 2 + r / 2;
+        if (isBadVersion(mid)) {
+	    std::cout << l << ", " << mid << std::endl;
+            return findBadVersion(l, mid);
+        } else {
+            return findBadVersion(mid + 1, r);
+	    std::cout << mid << ", " << r << std::endl;
+        }
+    }
+    
+public:
+    
+    int firstBadVersion(int n) {
+        return findBadVersion(0, n);
+    }
+};
+
+
+bool isBadVersion(int version) {
+	return version >= 1702766719;
+}
+
+int main() {
+	Solution s; 
+	std::cout << s.firstBadVersion(2126753390) << std::endl;
+}
